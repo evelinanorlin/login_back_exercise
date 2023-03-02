1673b5f84f4418003e0e23670bb04aa7f53b58c9
Evelina Norlin <evelina.norlin@hotmail.com>
2023-03-02 10:57:14 +0100

    built the project

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+let users = [
+  {id: 1, username: "Nicho", password: "fluga"},
+  {id: 2, username: "Eve", password: "mygga"},
+  {id: 3, username: "Ronja", password: "ben"}
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(users);
+});
+
+router.post('/', function(req, res, next) {
+  let newLogin = req.body;
+  let loginList = [];
+  let currentUser;
+
+  for (let i = 0; i < users.length; i++){
+    if(newLogin.username === users[i].username && newLogin.password === users[i].password){
+      loginList = ["User and password correct"];
+      currentUser = {id: users[i].id, username: users[i].username};
+    }
+  }
+
+  if (loginList.length === 1){
+    res.json({message: currentUser})
+    console.log("logged in")
+  } else{
+    res.json({message: "error"})
+    console.log("failed to login")
+  }
+});
+
+router.get('/newuser', function(req, res, next) {
+  res.json(users);
+});
+
+router.post('/newuser', function(req, res, next) {
+  let newUser = req.body;
+  newUser.id = users.length +1;
+  users.push(newUser);
+  res.json(users);
 });
 
 module.exports = router;
